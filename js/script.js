58a071234ce053b0daa214eb488cfd77be75c493
gabei <gabe1792@gmail.com>
2021-06-25 17:26:26 -0500

    added styling to the page, as well as some form inputs and additional code organization in scriot.js and style.css

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('script.js loaded...');
+
+
+
+
+/* Global Settings
+___________________________________*/
+const sketchContainer = document.querySelector('.sketch-container');
+const containerWidth = sketchContainer.offsetWidth;
+
+/* Pixel creation
+___________________________________*/
+function createPixel() {
+  //create a pixel of the sketch pad
+}
