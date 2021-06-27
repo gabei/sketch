df4bc02b1180eda22225230ff00d49d4ce20fba2
gabei <gabe1792@gmail.com>
2021-06-27 09:21:51 -0500

    finished settings ui, pre-sketch landing screen

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ console.log('script.js loaded...');
 
 
 
-/* Global Settings
+/* Canvas Settings
 ___________________________________*/
 const sketchContainer = document.querySelector('.sketch-container');
 const containerWidth = sketchContainer.offsetWidth;
+
+
+/* Setup Settings
+___________________________________*/
+const button = document.querySelector('.settings_button');
+let gridSize;
+button.on('click', function(e){
+  e.preventDefault();
+  
+});
 
 /* Pixel creation
 ___________________________________*/
